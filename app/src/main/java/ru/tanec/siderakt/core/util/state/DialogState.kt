5b9688e479421d7b935c55a363871cc69ae6d9bf
tanexc <artur.231456@gmail.com>
2023-08-30 22:54:40 +0300

    end test dialog edit

--- app/src/main/java/ru/tanec/siderakt/core/util/state/DialogState.kt
+++ app/src/main/java/ru/tanec/siderakt/core/util/state/DialogState.kt
@@ -1,10 +1,8 @@
 package ru.tanec.siderakt.core.util.state
 
 sealed class DialogState {
-
     data object Exit: DialogState()
     data object OpenLink: DialogState()
     data object FinishTest: DialogState()
     data object TestInfo: DialogState()
-
 }
